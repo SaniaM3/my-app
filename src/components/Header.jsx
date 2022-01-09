ca235444724604395ec9cc4319130977be1f2f6d
Aleksandr Masko <amasko18@gmail.com>
2022-01-09 21:16:57 +0300

    React

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Header = () => {
+    return <header className='header'>
+    <img className="image" src="https://gc.onliner.by/images/logo/onliner_logo.v3@2x.png?token=1639997396"></img>
+  </header>
+    
+}
+
+export default Header;
